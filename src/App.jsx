0687589c7eacb9946845848e7340834954f8e7ce
MarcoTuc <marco.tuccio95@gmail.com>
2025-02-20 12:42:38 +0100

    themetoggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './style.css'
 import PyGrid from './PyGrid'
+import { ThemeProvider } from './components/ThemeProvider'
+import ThemeToggle from './components/ThemeToggle'
 
 function App() {
   return (
-    <div>
-      <PyGrid />
-    </div>
+    <ThemeProvider>
+      <div>
+        <ThemeToggle />
+        <PyGrid />
+      </div>
+    </ThemeProvider>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,5 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
-    allowedHosts: ['74660cfd8d5aff.lhr.life']
   },
 })
